e2dfba268250cea6189ed692f4ccc77b39bc568d
Bogdan-K-A <kozhoma_b@ukr.net>
2021-09-29 20:41:58 +0300

    исправил

--- src/js/switch.js
+++ src/js/switch.js
@@ -2,40 +2,27 @@ import { DARK, LIGHT } from '../js/theme';
 
 const refs = {
   switchBtn: document.querySelector('#theme-switch-toggle'),
+  body: document.body,
 };
 
 refs.switchBtn.addEventListener('change', switchPosition);
 
-/* ------------------------ Функция переключения темы ----------------------- */
+/* ------------------------ Функции переключения темы ----------------------- */
 function switchPosition(e) {
-  /* ------------------- Фиксируем ключ темы в localStorage ------------------- */
-  let switchValue = e.target.checked;
-  console.log(switchValue);
-  switchValue
-    ? localStorage.setItem('theme', DARK)
-    : localStorage.setItem('theme', LIGHT);
-
-  /* -------------- Меняем цвет темы при нажатии на переключатель ------------- */
-  /* ----------------------- убрал лишний код (=== true)----------------------- */
-  if (switchValue) {
-    changeThemeToBlack();
-  } else {
-    changeThemeToWhite();
-  }
+  e.target.checked
+    ? changeThemeColor(DARK, LIGHT)
+    : changeThemeColor(LIGHT, DARK);
 }
 
-/* ------------------- вынес параметры в отдельный функции ------------------ */
-const changeThemeToBlack = function () {
-  localStorage.setItem('theme', DARK);
-  document.querySelector('body').classList.add(DARK);
-  document.querySelector('body').classList.remove(LIGHT);
-};
+function changeThemeColor(addColor, removeColor) {
+  refs.body.classList.remove(removeColor);
+  refs.body.classList.add(addColor);
+  addThemeColorInLocalStorage(addColor);
+}
 
-const changeThemeToWhite = function () {
-  localStorage.setItem('theme', LIGHT);
-  document.querySelector('body').classList.add(LIGHT);
-  document.querySelector('body').classList.remove(DARK);
-};
+function addThemeColorInLocalStorage(color) {
+  localStorage.setItem('theme', color);
+}
 
 /* ----------------- Сохраняем тему при обновлении страницы ----------------- */
 let saveSwitchPosition = localStorage.getItem('theme');
@@ -44,7 +31,6 @@ if (!saveSwitchPosition) {
   saveSwitchPosition = LIGHT;
   localStorage.setItem('theme', saveSwitchPosition);
 }
-document.querySelector('body').classList.add(saveSwitchPosition);
 
-/* ------------- убрал (? false : true;) / LIGHT заменил на DARK ------------ */
+refs.body.classList.add(saveSwitchPosition);
 refs.switchBtn.checked = saveSwitchPosition === DARK;
